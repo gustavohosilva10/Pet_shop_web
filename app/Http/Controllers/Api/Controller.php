f4a70542f9fa1fdec037cdaecd9288a9bdeafc5c
Gustavo henrique oliveira da silva <47875693+gustavohosilva10@users.noreply.github.com>
2021-04-08 15:41:42 -0300

    Update Controller.php

--- app/Http/Controllers/Api/Controller.php
+++ app/Http/Controllers/Api/Controller.php
@@ -1,21 +1,5 @@
 <?php
 
-/* namespace App\Http\Controllers\Api;
-
-use Illuminate\Support\Facades\Log;
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Http\Request;
-use Illuminate\Routing\Controller as BaseController;
-
-class Controller extends BaseController
-{
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-}
-
-<?php */
-
 namespace App\Http\Controllers\Api;
 
 use Illuminate\Support\Facades\Log;
